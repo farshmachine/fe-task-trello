b8f075ad88c3f32bc8b9b8a85c0e2792e0def699
farshmachine <m4ghty@yandex.ru>
2022-03-27 15:04:52 +0300

    implemented logic for adding card

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
+import { useCallback, useState } from 'react';
 import CardForm from './components/CardForm/CardForm';
 import CardList from './components/CardList/CardList';
+import { ICard } from './components/Card/Card';
 import './app.css';
 
+const INITIAL_POS = 65535;
+
 function App() {
+  const [cards, setCards] = useState<Array<ICard>>([]);
+
+  const addCard = useCallback(
+    (content) => {
+      const lastCard = cards[cards.length - 1];
+      const cardPos = lastCard ? lastCard.pos + INITIAL_POS + 1 : INITIAL_POS;
+      setCards(cards.concat({ content, pos: cardPos }));
+    },
+    [cards],
+  );
   return (
     <main className="app">
-      <CardList items={[{ content: 'Hello app', pos: 65535 }, { content: 'Hello card', pos: 165535 }]} />
-      <CardForm />
+      <CardList items={cards} />
+      <CardForm onSubmit={addCard} />
     </main>
   );
 }
--- src/components/CardForm/CardForm.tsx
+++ src/components/CardForm/CardForm.tsx
@@ -1,9 +1,26 @@
+import { FormEvent, useCallback } from 'react';
 import './cardform.css';
 
-export default function CardForm() {
+interface ICardFormProps {
+  onSubmit: (v: string) => void;
+}
+
+type TFormTarget = HTMLFormElement & { text: HTMLInputElement };
+
+export default function CardForm({ onSubmit }: ICardFormProps) {
+  const handleSubmit = useCallback(
+    (e: FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      const input = (e.target as TFormTarget).text;
+      onSubmit(input.value);
+      input.value = '';
+    },
+    [onSubmit],
+  );
+
   return (
-    <form className="form">
-      <textarea className="input" />
+    <form className="form" onSubmit={handleSubmit}>
+      <textarea className="input" name="text" />
       <button type="submit" className="button">Add</button>
     </form>
   );
